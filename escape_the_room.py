c5604c611fb72db2e9d9e9c9806c17436d94e50f
Kavitha <kavitha@traveltripper.com>
2023-10-25 20:52:17 -0400

    comment for room

--- escape_the_room.py
+++ escape_the_room.py
@@ -7,6 +7,9 @@ import sys
 
 @dataclass
 class room(Enum):
+    """
+    This has all the rooms for this game. Each room has 6 attributes, s, n, e, w, hint and picture
+    """
     foodcourt = {
         "s": "outcropping",
         "n": "wall",
